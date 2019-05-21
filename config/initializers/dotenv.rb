53561da807493a018898da92c793e8e2e8937677
Jon Gutwillig <programcodex@gmail.com>
2019-05-21 14:49:07 -0700

    ignoring the .env files

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,13 +1,16 @@
-Dotenv.require_keys(
-	'DEVISE_SECRET_KEY',
-    'PUSHER_APP_ID',
-    'PUSHER_KEY',
-    'PUSHER_SECRET',
-    'PUSHER_CLUSTER',
-    'TOKEN_EXPIRATION',
-    'TOKEN_KEY_BASE'
-)
+if Rails.env.development? || Rails.env.test?
 
+	Dotenv.require_keys(
+		'DEVISE_SECRET_KEY',
+	    'PUSHER_APP_ID',
+	    'PUSHER_KEY',
+	    'PUSHER_SECRET',
+	    'PUSHER_CLUSTER',
+	    'TOKEN_EXPIRATION',
+	    'TOKEN_KEY_BASE'
+	)
+
+end
 
 TOKEN_EXPIRATION = ENV['TOKEN_EXPIRATION']
 TOKEN_KEY_BASE = ENV['TOKEN_KEY_BASE']
