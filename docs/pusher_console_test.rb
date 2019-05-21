ca4138d9ed330f6b7b05ceb18ca6458ee8d0441b
Jon Gutwillig <programcodex@gmail.com>
2019-05-21 13:29:58 -0700

    adding faviconfor MI

--- docs/pusher_console_test.rb
+++ docs/pusher_console_test.rb
@@ -1,4 +1,8 @@
 # app/controllers/hello_world_controller.rb
+
+!!!!!  Use :ptest in rails console !!!!!
+
+
 class HelloWorldController < ApplicationController
     def hello_world
         Pusher.trigger('my-channel', 'my-event', { message: 'it works!!' })
