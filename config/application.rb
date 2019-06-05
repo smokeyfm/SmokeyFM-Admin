c772a44b3469a6f6ac36fb4d600bbb9f08391406
Jon Gutwillig <programcodex@gmail.com>
2019-06-05 11:24:17 -0700

    re-install DB data file re-writes

--- config/application.rb
+++ config/application.rb
@@ -8,23 +8,23 @@ Bundler.require(*Rails.groups)
 
 module DnaAdmin
     class Application < Rails::Application
-        config.to_prepare do
-            # Load application's model / class decorators
-            Dir.glob(
-                File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')
-            ) { |c| Rails.configuration.cache_classes ? require(c) : load(c) }
+      	config.to_prepare do
+          	# Load application's model / class decorators
+          	Dir.glob(
+              	File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')
+          	) { |c| Rails.configuration.cache_classes ? require(c) : load(c) }
 
-            # Load application's view overrides
-            Dir.glob(
-                File.join(File.dirname(__FILE__), '../app/overrides/*.rb')
-            ) { |c| Rails.configuration.cache_classes ? require(c) : load(c) }
-        end
-        # Initialize configuration defaults for originally generated Rails version.
-        config.load_defaults 5.2
+          	# Load application's view overrides
+          	Dir.glob(
+              	File.join(File.dirname(__FILE__), '../app/overrides/*.rb')
+          	) { |c| Rails.configuration.cache_classes ? require(c) : load(c) }
+      	end
+      	# Initialize configuration defaults for originally generated Rails version.
+      	config.load_defaults 5.2
 
-        # Settings in config/environments/* take precedence over those specified here.
-        # Application configuration can go into files in config/initializers
-        # -- all .rb files in that directory are automatically loaded after loading
-        # the framework and any gems in your application.
+      	# Settings in config/environments/* take precedence over those specified here.
+      	# Application configuration can go into files in config/initializers
+      	# -- all .rb files in that directory are automatically loaded after loading
+      	# the framework and any gems in your application.
     end
 end
