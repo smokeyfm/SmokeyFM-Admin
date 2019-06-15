c48cd6b8f63b8a9767f1304d5fa06010ea96f0e0
pfeinman <peter.feinman@gmail.com>
2019-06-14 20:21:40 -0700

    delete unnecessary comment and fix merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-    # This line mounts Spree's routes at the root of your application.
-    # This means, any requests to URLs such as /products, will go to
-    # Spree::ProductsController.
-    # If you would like to change where this engine is mounted, simply change the
-    # :at option to something different.
-    #
-    # We ask that you don't use the :as option here, as Spree relies on it being
-    # the default of "spree".
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount Spree::Core::Engine, at: '/'
+  
 end
 
 Spree::Core::Engine.add_routes do
@@ -18,10 +12,7 @@ Spree::Core::Engine.add_routes do
 
     get "/messages" => "messages#index"
     get "/messages/support" => "messages#message_support"
-
-    # kludge
-    get "/messages/one" => "messages#thread_list_one"
-    get "/messages/two" => "messages#thread_list_two"
+    
   end
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
