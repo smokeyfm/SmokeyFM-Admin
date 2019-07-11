e2c66ef7ed3e41e0bc6a34c0de0b02cdc6ad6f14
Aaron Smulktis <aaronsmulktis@gmail.com>
2019-07-10 17:43:36 -0700

    add TWILIO to env.example, add static_content extension, tweak README, redirect root to login, redirect after login to adminy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,11 @@
-class ApplicationController < ActionController::Base; end
+class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if spree_current_user.has_spree_role?("admin")
+      admin_path
+    # elsif spree_current_user.has_spree_role?("designer")
+    #   '/designers/spree_variants/new' #rails helper gives the wrong path, not sure why
+    else
+      root_path
+    end
+  end
+end
