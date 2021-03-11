ace3461c1c69ebb1da5d44336dab30d7cf286c38
David Dombrowsky <davek@6thstreetradio.org>
2021-03-11 00:01:50 -0500

    still trying to get sample images to load

--- docker-build.sh
+++ docker-build.sh
@@ -9,9 +9,15 @@ docker-compose build
 docker-compose up -d
 
 docker-compose exec web rails db:create db:schema:load db:migrate
-docker-compose exec -e ADMIN_EMAIL=spree@example.com -e ADMIN_PASSWORD=spree123 web rails db:seed
+docker-compose exec \
+    -e ADMIN_EMAIL=spree@example.com \
+    -e ADMIN_PASSWORD=spree123 \
+    web bundle exec rails db:seed
 
-docker-compose exec web rails spree_sample:load
+docker-compose exec \
+    -e SKIP_SAMPLE_IMAGES=false \
+    web bundle exec rake spree_sample:load
+
 docker-compose restart web
 
 # to follow logs...
