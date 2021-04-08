9750a31241ddaeba7ca19b707e5124f7c76fe2ea
David Dombrowsky <davek@6thstreetradio.org>
2021-04-08 04:02:09 +0000

    fixed bucket name env, added k8 deploy script

---
+++ k8-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+# Save built docker image and push it to k8 cache.
+#
+# This won't be necessary if it is pushed to dockerhub
+# or another registry server.
+
+docker-compose build
+docker tag dna-admin_web dna-admin:0.1
+docker save dna-admin > dna-admin.tar
+microk8s ctr image import dna-admin.tar
+
+kubectl delete deployment.apps/dna-admin || :
+kubectl apply -f dna-k8.yaml
