fa55c3ccfe1502e50790c24088674a2c4cb5659e
David Dombrowsky <davek@6thstreetradio.org>
2021-04-08 15:26:25 -0400

    compile assets once on container start

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,12 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+echo RAILS_ENV=$RAILS_ENV
+
+if [ ! -e assets_precompiled ] ; then
+    rails assets:precompile
+    touch assets_precompiled
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
