e39c52440bb039ec8f8af5f59517c5710419700e
mayankg-protonshub <mayank.g@protonshub.com>
2021-07-12 15:03:57 +0530

    fetch page api using slug

--- app/controllers/spree/api/v1/pages_controller.rb
+++ app/controllers/spree/api/v1/pages_controller.rb
@@ -160,7 +160,71 @@ class Spree::Api::V1::PagesController < ApplicationController
     }
     singular_success_model(200, Spree.t('page.success.page_list'), response_data)
   end
-
+  swagger_path "/pages/{slug}" do
+    operation :get do
+      key :summary, "Fetch page using slug"
+      key :description, "Fetch page using slug"
+      key :tags, [
+        'Pages'
+      ]
+      parameter do
+        key :name, :slug
+        key :in, :path
+        key :description, 'slug of page to fetch'
+        key :required, true
+        key :type, :string
+      end
+      response 200 do
+        key :description, "Successfull"
+        schema do
+          key :'$ref', :single_page_response
+        end
+      end
+      response 400 do
+        key :description, "Error"
+        schema do
+          key :'$ref', :common_response_model
+        end
+      end
+    end
+  end
+  swagger_schema :single_page_response do
+    key :required, [:response_code, :response_message]
+    property :response_code do
+      key :type, :integer
+    end
+    property :response_message do
+      key :type, :string
+    end
+    property :response_data do
+      key :'$ref', :page
+    end
+  end
   def show
+    page = Spree::Page.find_by_slug(params[:slug])
+    if page.present?
+      response_data = {
+        id: page&.id || 0,
+        title: page&.title || "",
+        body: page&.body || "",
+        slug: page&.slug || "",
+        created_at: to_timestamp(page&.created_at) || 0,
+        updated_at: to_timestamp(page&.updated_at) || 0,
+        show_in_header: page&.show_in_header || false,
+        foreign_link: page&.foreign_link || "",
+        position: page&.position || 0,
+        visible: page&.visible || false,
+        meta_keywords: page&.meta_keywords || "",
+        meta_description: page&.meta_description || "",
+        layout: page&.layout || "",
+        show_in_sidebar: page&.show_in_sidebar || false,
+        meta_title: page&.meta_title || "",
+        render_layout_as_partial: page&.render_layout_as_partial || false,
+        show_in_footer: page&.show_in_footer || false
+      }
+      singular_success_model(200, Spree.t('page.success.show'), response_data)
+    else
+      error_model(400, Spree.t('page.error.page_not_found'))
+    end
   end
 end
