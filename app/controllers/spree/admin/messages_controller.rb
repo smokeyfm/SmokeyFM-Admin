a14c2fdee4167bbf1c8db64e95e55ac9b82993ab
mayankg-protonshub <mayank.g@protonshub.com>
2021-07-21 14:45:23 +0530

    Add message field in strong params

--- app/controllers/spree/admin/messages_controller.rb
+++ app/controllers/spree/admin/messages_controller.rb
@@ -60,7 +60,7 @@ class Spree::Admin::MessagesController <  Spree::Admin::BaseController
 		session[:return_to] = request.url
 	end
 	def message_params
-		params.require(:message).permit(:is_received, :is_read, :sentiment, :sender_type, :sender_id, :receiver_type, :receiver_id)
+		params.require(:message).permit(:is_received, :is_read, :sentiment, :sender_type, :sender_id, :receiver_type, :receiver_id, :message)
 	end
 	def collection(resource)
 		return @collection if @collection.present?
