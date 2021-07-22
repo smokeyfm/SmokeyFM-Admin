ef96869a0481c2904f1b527dc086f8d675804571
mayankg-protonshub <mayank.g@protonshub.com>
2021-07-22 11:30:59 +0530

    Add association for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   has_many :sent_messages, class_name: 'Message',as: :sender, dependent: :destroy
   has_many :received_messages, class_name: 'Message', as: :receiver, dependent: :destroy
+  belongs_to :actor, class_name: 'Spree::User'
 end
