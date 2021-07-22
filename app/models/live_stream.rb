051947102c22dac35c0c818746004bae7b6f0c9c
mayankg-protonshub <mayank.g@protonshub.com>
2021-07-22 14:17:56 +0530

    Add callback

--- app/models/live_stream.rb
+++ app/models/live_stream.rb
@@ -1,4 +1,11 @@
 class LiveStream < ApplicationRecord
   has_many :live_stream_products, dependent: :destroy
   has_many :products, class_name: 'Spree::Product', through: :live_stream_products, dependent: :destroy
+  belongs_to :thread, class_name: "ThreadTable", optional: :true
+
+  after_create :assign_thread_id
+  def assign_thread_id
+    thread_table = ThreadTable.create(stale: true, archived: true)
+    self.update(thread_table_id: thread_table.id)
+  end
 end
